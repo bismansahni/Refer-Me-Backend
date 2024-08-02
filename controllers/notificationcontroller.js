7299640484204bfda488e7a326094c637df0191c
bismansahni <bismansahni@outlook.com>
2024-08-03 01:04:06 +0530

    added separate notif

--- controllers/notificationcontroller.js
+++ controllers/notificationcontroller.js
@@ -1,20 +1,44 @@
-import { getNotifications } from '../services/notificationservices.js';
+// import { getNotifications } from '../services/notificationservices.js';
+
+// // export const getUserNotifications = (req, res) => {
+// //   const notifications = getNotifications(req.user.id);
+// //   res.status(200).json({ notifications });
+// // };
 
 // export const getUserNotifications = (req, res) => {
 //   const notifications = getNotifications(req.user.id);
-//   res.status(200).json({ notifications });
+
+//   const response = notifications.map(notification => ({
+//     message: notification.message,
+//     referralRequest: notification.referralDetails.referralRequest,
+//     user: notification.referralDetails.user,
+//     target_job: notification.referralDetails.target_job,
+//     target_company: notification.referralDetails.target_company,
+//   }));
+
+//   res.status(200).json({ notifications: response });
 // };
 
-export const getUserNotifications = (req, res) => {
-  const notifications = getNotifications(req.user.id);
 
-  const response = notifications.map(notification => ({
-    message: notification.message,
-    referralRequest: notification.referralDetails.referralRequest,
-    user: notification.referralDetails.user,
-    target_job: notification.referralDetails.target_job,
-    target_company: notification.referralDetails.target_company,
-  }));
 
-  res.status(200).json({ notifications: response });
+
+import Notification from '../models/notification_model.js';
+
+export const getUserNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({ userId: req.user.id });
+
+    const response = notifications.map(notification => ({
+      message: notification.message,
+      referralRequest: notification.referralDetails.referralRequest,
+      user: notification.referralDetails.referralRequest.user,
+      target_job: notification.referralDetails.target_job,
+      target_company: notification.referralDetails.target_company,
+    }));
+
+    res.status(200).json({ notifications: response });
+  } catch (error) {
+    console.error('Failed to fetch notifications:', error);
+    res.status(500).json({ error: 'Failed to fetch notifications' });
+  }
 };
